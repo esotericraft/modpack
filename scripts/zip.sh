b0469627622fd52ebb4ad68951cc8e700c47473c
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-15 22:20:18 +0000

    Zip folder correctly

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,1 +1,1 @@
-zip "Esoteric Defaults.mrpack" .idea .vscode README.md LICENSE IDEAS.md ISSUES.md docs CHANGELOG.md CODE_OF_CONDUCT.md .assets .gitignore modrinth.index.json overrides
+zip "Esoteric Defaults.mrpack" .idea/* .vscode/* README.md LICENSE IDEAS.md ISSUES.md docs/* CHANGELOG.md CODE_OF_CONDUCT.md .assets/* .gitignore modrinth.index.json overrides/*
