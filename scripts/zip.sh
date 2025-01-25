98532b50b28de98a73a507bdd6817bf123e62581
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-25 20:29:27 +0000

    Update zipped file name

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,1 +1,1 @@
-zip -r "Esoteric Defaults.mrpack" .idea/ .vscode/ README.md LICENSE IDEAS.md ISSUES.md docs/ CHANGELOG.md CODE_OF_CONDUCT.md .assets/ .gitignore modrinth.index.json overrides/
+zip -r "EsoteriCraft.mrpack" .idea/ .vscode/ README.md LICENSE IDEAS.md ISSUES.md docs/ CHANGELOG.md CODE_OF_CONDUCT.md .assets/ .gitignore modrinth.index.json overrides/
